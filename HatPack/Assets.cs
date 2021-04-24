7d01e48f3532127a9a0f1c6ace7e7919215f1cad
Matthew Carrier <matthew.s.carrier.civ@mail.mil>
2021-04-24 15:44:26 -0400

    Add project files.

---
+++ HatPack/Assets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using Reactor.Extensions;
+using UnityEngine;
+
+namespace HatPack
+{
+    public class Assets
+    {
+        private static AssetBundle AssetBundle;
+
+        public static void LoadAssetBundle()
+        {
+            //FileLog.logPath = @"C:\AmongUsModded\BepInEx\ModLogs\BunkerLog.log";
+            //FileLog.Log("Loading Asset Bundle");
+            byte[] bundleRead = Assembly.GetCallingAssembly().GetManifestResourceStream("HatPack.HatPackAssets.assetbundle").ReadFully();
+            AssetBundle = AssetBundle.LoadFromMemory(bundleRead);
+            //FileLog.Log("Asset Bundle Loaded");
+
+        }
+
+        public static UnityEngine.Object LoadAsset(string name)
+            => AssetBundle.LoadAsset(name);
+    }
+}
+
---
+++ HatPack/HatPackPlugin.cs
@@ -0,0 +1,91 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Reactor;
+using System.Linq;
+using UnityEngine;
+
+namespace HatPack
+{
+    [BepInPlugin(Id , "HatPack", "1.0.0.0")]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    public class HatPackPlugin : BasePlugin
+    {
+
+        public const string Id = "hats.pack";
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+
+        public override void Load()
+        {
+            Assets.LoadAssetBundle();
+            Harmony.PatchAll();
+        }
+        private static bool _customHatsLoaded = false;        
+        [HarmonyPatch(typeof(HatManager), nameof(HatManager.GetHatById))]
+        public static class AddCustomHats
+        {
+            public static void Prefix(PlayerControl __instance)
+            {
+                //FileLog.logPath = @"C:\AmongUsModded\BepInEx\ModLogs\BunkerLog.log";
+                if (!_customHatsLoaded)
+                {
+                    var allHats = HatManager.Instance.AllHats;
+
+                    var customHatNames = new[] { "panda", "carrot", "raddish", "reaper" };
+                    string[] climbHatNames = { "reaper.climb" };
+                    string[] floorHatNames = { "reaper.dead" };
+                    //FileLog.Log($"Number of hats : { customHatNames.Length}");
+                    
+                    foreach (string hatName in customHatNames)
+                    {
+                        if (floorHatNames.Contains($"{hatName}.dead") && climbHatNames.Contains($"{hatName}.climb"))
+                        {
+                            string floorHat = $"{hatName}.dead";
+                            string climbHat = $"{hatName}.climb";
+
+                            //FileLog.Log($"Climbhat : {climbHat}");
+                            //FileLog.Log($"Floorhat : {floorHat}");
+                            //FileLog.Log($"Creating : {hatName}");
+                            allHats.Add(CreateHat(GetSprite(hatName), GetSprite(climbHat), GetSprite(floorHat)));
+                            //FileLog.Log($"Hat Created : {hatName}");
+                        }
+                        else
+                        {
+                            //FileLog.Log($"Creating : {hatName}");
+                            allHats.Add(CreateHat(GetSprite(hatName)));
+                            //FileLog.Log($"Hat Created : {hatName}");
+                        }
+
+                        
+                        _customHatsLoaded = true;
+                    }
+
+                    
+
+                    _customHatsLoaded = true;
+                    //FileLog.Log($"CustomHats Loaded : {_customHatsLoaded}");
+                }
+            }
+
+            private static Sprite GetSprite(string name)
+                => Assets.LoadAsset(name).Cast<GameObject>().GetComponent<SpriteRenderer>().sprite;
+
+            private static HatBehaviour CreateHat(Sprite sprite, Sprite climb = null, Sprite floor = null)
+            {                
+
+                return new HatBehaviour
+                {
+                    MainImage = sprite,
+                    ProductId = $"hat_{sprite.name}",
+                    InFront = true,
+                    NoBounce = true,
+                    ClimbImage = climb,
+                    FloorImage = floor
+                };
+            }
+        }
+
+    }
+}
